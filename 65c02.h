3eda01b5162ddb180c38b4f45fad114d2dedecba
Joshua Scholar <joshuascholar@gmail.com>
2019-10-26 04:31:30 -0700

    Finished in-memory assembler
    but nothing is tested yet

--- 65c02.h
+++ 65c02.h
@@ -356,6 +356,189 @@ struct emulate65c02 {
 	void _trb_abs(int v) { comp_byte(0x1c); comp_word(v); }
 	void _ora_abx(int v) { comp_byte(0x1d); comp_word(v); }
 	void _asl_abx(int v) { comp_byte(0x1e); comp_word(v); }
+	void _jsr(Label& label) {
+		comp_byte(0x20);
+		if (label.has_target()) {
+			comp_word(label.target);
+		}
+		else {
+			label.add_fixup(compile_point, false);
+			comp_word((compile_point+2)&0xffff);
+		}
+	}
+	void _and_izx(int v) { comp_byte(0x21); comp_byte(v); }
+	void _bit_zp(int v) { comp_byte(0x24); comp_byte(v); }
+	void _and_zp(int v) { comp_byte(0x25); comp_byte(v); }
+	void _rol_zp(int v) { comp_byte(0x26); comp_byte(v); }
+	void _plp() { comp_byte(0x28); }
+	void _and_imm(int v) { comp_byte(0x29); comp_byte(v); }
+	void _rol() { comp_byte(0x2a); }
+	void _bit_abs(int v) { comp_byte(0x2c); comp_word(v); }
+	void _and_abs(int v) { comp_byte(0x2d); comp_word(v); }
+	void _rol_abs(int v) { comp_byte(0x2e); comp_word(v); }
+	void _bmi(Label& label, bool force_short = false) {
+		compile_branch(0x30, 0x10, label, force_short);
+	}
+	void _and_izy(int v) { comp_byte(0x31); comp_byte(v); }
+	void _and_izp(int v) { comp_byte(0x32); comp_byte(v); }
+	void _bit_zpx(int v) { comp_byte(0x34); comp_byte(v); }
+	void _and_zpx(int v){ comp_byte(0x35); comp_byte(v); }
+	void _rol_zpx(int v){ comp_byte(0x36); comp_byte(v); }
+	void _sec(){ comp_byte(0x38); }
+	void _and_aby(int v){ comp_byte(0x39); comp_word(v); }
+	void _dec(){ comp_byte(0x3a); }
+	void _bit_abx(int v){ comp_byte(0x3c); comp_word(v); }
+	void _and_abx(int v){ comp_byte(0x3d); comp_word(v); }
+	void _rol_abx(int v){ comp_byte(0x3e); comp_word(v); }
+	void _rti(int v){ comp_byte(0x40); }
+	void _eor_izx(int v){ comp_byte(0x41); comp_byte(v); }
+	void _eor_zp(int v){ comp_byte(0x45); comp_byte(v); }
+	void _lsr_zp(int v){ comp_byte(0x46); comp_byte(v); }
+	void _pha(int v){ comp_byte(0x48); }
+	void _eor_imm(int v){ comp_byte(0x49); comp_byte(v); }
+	void _lsr(int v){ comp_byte(0x4a); }
+	void _jmp_abs(int v){ comp_byte(0x4c); comp_word(v); }
+	void _eor_abs(int v){ comp_byte(0x4d); comp_word(v); }
+	void _lsr_abs(int v){ comp_byte(0x4e); comp_word(v); }
+	void _bvc(Label &label, bool force_short=false){ compile_branch(0x50, 0x70, label, force_short); }
+	void _eor_izy(int v){ comp_byte(0x51); comp_byte(v); }
+	void _eor_izp(int v){ comp_byte(0x52); comp_byte(v); }
+	void _eor_zpx(int v){ comp_byte(0x55); comp_byte(v); }
+	void _lsr_zpx(int v){ comp_byte(0x56); comp_byte(v); }
+	void _cli(int v){ comp_byte(0x58); }
+	void _eor_aby(int v){ comp_byte(0x59); comp_word(v); }
+	void _phy(int v){ comp_byte(0x5a); }
+	void _eor_abx(int v){ comp_byte(0x5d); comp_word(v); }
+	void _lsr_abx(int v){ comp_byte(0x5e); comp_word(v); }
+	void _rts(int v){ comp_byte(0x60); }
+	void _adc_izx(int v){ comp_byte(0x61); comp_byte(v); }
+	void _stz_zp(int v){ comp_byte(0x64); comp_byte(v); }
+	void _adc_zp(int v){ comp_byte(0x65); comp_byte(v); }
+	void _ror_zp(int v){ comp_byte(0x66); comp_byte(v); }
+	void _pla(int v){ comp_byte(0x68); }
+	void _adc_imm(int v){ comp_byte(0x69); comp_byte(v); }
+	void _ror(int v){ comp_byte(0x6a); }
+	void _jmp_ind(int v){ comp_byte(0x6c); comp_word(v); }
+	void _adc_abs(int v){ comp_byte(0x6d); comp_word(v); }
+	void _ror_abs(int v){ comp_byte(0x6e); comp_word(v); }
+	void _bvs(Label &label, bool force_short = false) { compile_branch(0x70, 0x50, label, force_short); }
+	void _adc_izy(int v){ comp_byte(0x71); comp_byte(v); }
+	void _adc_izp(int v){ comp_byte(0x72); comp_byte(v); }
+	void _stz_zpx(int v){ comp_byte(0x74); comp_byte(v); }
+	void _adc_zpx(int v){ comp_byte(0x75); comp_byte(v); }
+	void _ror_zpx(int v){ comp_byte(0x76); comp_byte(v); }
+	void _sei(int v){ comp_byte(0x78); }
+	void _adc_aby(int v){ comp_byte(0x79); comp_word(v); }
+	void _ply(int v){ comp_byte(0x7a); }
+	void _jmp_iax(int v){ comp_byte(0x7c); comp_word(v); }
+	void _adc_abx(int v){ comp_byte(0x7d); comp_word(v); }
+	void _ror_abx(int v){ comp_byte(0x7e); comp_word(v); }
+	void _bra(Label& label, bool force_short) {
+		int cur_add = (compile_point + 2) & 0xffff;
+		if (label.has_target() && label.target - cur_add <= 128 && cur_add - label.target <= 127) {
+			comp_byte(0x80);
+			comp_byte(cur_add - label.target);
+		}
+		else if (force_short) {
+			label.add_fixup((cur_add - 1) & 0xffff, true);
+			comp_byte(0x80);
+			comp_byte(0);
+		}
+		else {
+			comp_byte(0x4c);//jmp
+			label.add_fixup(compile_point, false);
+			comp_word((compile_point + 2) & 0xffff);
+		}
+	}
+	void _sta_izx(int v){ comp_byte(0x81); comp_byte(v); }
+	void _sty_zp(int v){ comp_byte(0x84); comp_byte(v); }
+	void _sta_zp(int v){ comp_byte(0x85); comp_byte(v); }
+	void _stx_zp(int v){ comp_byte(0x86); comp_byte(v); }
+	void _dey(int v){ comp_byte(0x88); }
+	void _bit_imm(int v){ comp_byte(0x89); comp_byte(v); }
+	void _txa(int v){ comp_byte(0x8a); }
+	void _sty_abs(int v){ comp_byte(0x8c); comp_word(v); }
+	void _sta_abs(int v){ comp_byte(0x8d); comp_word(v); }
+	void _stx_abs(int v){ comp_byte(0x8e); comp_word(v); }
+	void _bcc(Label &label, bool force_short = false) { compile_branch(0x90, 0xb0, label, force_short); }
+	void _sta_izy(int v){ comp_byte(0x91); comp_byte(v); }
+	void _sta_izp(int v){ comp_byte(0x92); comp_byte(v); }
+	void _sty_zpx(int v){ comp_byte(0x94); comp_byte(v); }
+	void _sta_zpx(int v){ comp_byte(0x95); comp_byte(v); }
+	void _stx_zpy(int v){ comp_byte(0x96); comp_byte(v); }
+	void _tya(int v){ comp_byte(0x98); }
+	void _sta_aby(int v){ comp_byte(0x99); comp_word(v); }
+	void _txs(int v){ comp_byte(0x9a); }
+	void _stz_abs(int v){ comp_byte(0x9c); comp_word(v); }
+	void _sta_abx(int v){ comp_byte(0x9d); comp_word(v); }
+	void _stz_abx(int v){ comp_byte(0x9e); comp_word(v); }
+	void _ldy_imm(int v){ comp_byte(0xa0); comp_byte(v); }
+	void _lda_izx(int v){ comp_byte(0xa1); comp_byte(v); }
+	void _ldx_imm(int v){ comp_byte(0xa2); comp_byte(v); }
+	void _ldy_zp(int v){ comp_byte(0xa4); comp_byte(v); }
+	void _lda_zp(int v){ comp_byte(0xa5); comp_byte(v); }
+	void _ldx_zp(int v){ comp_byte(0xa6); comp_byte(v); }
+	void _tay(int v){ comp_byte(0xa8); }
+	void _lda_imm(int v){ comp_byte(0xa9); comp_byte(v); }
+	void _tax(int v){ comp_byte(0xaa); }
+	void _ldy_abs(int v){ comp_byte(0xac); comp_word(v); }
+	void _lda_abs(int v){ comp_byte(0xad); comp_word(v); }
+	void _ldx_abs(int v){ comp_byte(0xae); comp_word(v); }
+	void _bcs_relb0(Label &label, bool force_short = false) { compile_branch(0xb0, 0x90, label, force_short); }
+	void _lda_izy(int v){ comp_byte(0xb1); comp_byte(v); }
+	void _lda_izp(int v){ comp_byte(0xb2); comp_byte(v); }
+	void _ldy_zpx(int v){ comp_byte(0xb4); comp_byte(v); }
+	void _lda_zpx(int v){ comp_byte(0xb5); comp_byte(v); }
+	void _ldx_zpy(int v){ comp_byte(0xb6); comp_byte(v); }
+	void _clv(int v){ comp_byte(0xb8); }
+	void _lda_aby(int v){ comp_byte(0xb9); comp_word(v); }
+	void _tsx(int v){ comp_byte(0xba); }
+	void _ldy_abx(int v){ comp_byte(0xbc); comp_word(v); }
+	void _lda_abx(int v){ comp_byte(0xbd); comp_word(v); }
+	void _ldx_aby(int v){ comp_byte(0xbe); comp_word(v); }
+	void _cpy_imm(int v){ comp_byte(0xc0); comp_byte(v); }
+	void _cmp_izx(int v){ comp_byte(0xc1); comp_byte(v); }
+	void _cpy_zp(int v){ comp_byte(0xc4); comp_byte(v); }
+	void _cmp_zp(int v){ comp_byte(0xc5); comp_byte(v); }
+	void _dec_zp(int v){ comp_byte(0xc6); comp_byte(v); }
+	void _iny(int v){ comp_byte(0xc8); }
+	void _cmp_imm(int v){ comp_byte(0xc9); comp_byte(v); }
+	void _dex(int v){ comp_byte(0xca); }
+	void _wai(int v){ comp_byte(0xcb); }
+	void _cpy_abs(int v){ comp_byte(0xcc); comp_word(v); }
+	void _cmp_abs(int v){ comp_byte(0xcd); comp_word(v); }
+	void _dec_abs(int v){ comp_byte(0xce); comp_word(v); }
+	void _bne(Label &label, bool force_short = false) { compile_branch(0xd0, 0xf0, label, force_short); }
+	void _cmp_izy(int v){ comp_byte(0xd1); comp_byte(v); }
+	void _cmp_izp(int v){ comp_byte(0xd2); comp_byte(v); }
+	void _cmp_zpx(int v){ comp_byte(0xd5); comp_byte(v); }
+	void _dec_zpx(int v){ comp_byte(0xd6); comp_byte(v); }
+	void _cld(int v){ comp_byte(0xd8); }
+	void _cmp_aby(int v){ comp_byte(0xd9); comp_word(v); }
+	void _phx(int v){ comp_byte(0xda); }
+	void _stp(int v){ comp_byte(0xdb); }
+	void _cmp_abx(int v){ comp_byte(0xdd); comp_word(v); }
+	void _dec_abx(int v){ comp_byte(0xde); comp_word(v); }
+	void _cpx_imm(int v){ comp_byte(0xe0); comp_byte(v); }
+	void _sbc_izx(int v){ comp_byte(0xe1); comp_byte(v); }
+	void _cpx_zp(int v){ comp_byte(0xe4); comp_byte(v); }
+	void _sbc_zp(int v){ comp_byte(0xe5); comp_byte(v); }
+	void _inc_zp(int v){ comp_byte(0xe6); comp_byte(v); }
+	void _inx(int v){ comp_byte(0xe8); }
+	void _sbc_imm(int v){ comp_byte(0xe9); comp_byte(v); }
+	void _cpx_abs(int v){ comp_byte(0xec); comp_word(v); }
+	void _sbc_abs(int v){ comp_byte(0xed); comp_word(v); }
+	void _inc_abs(int v){ comp_byte(0xee); comp_word(v); }
+	void _beq(Label &label, bool force_short = false) { compile_branch(0xf0, 0xd0, label, force_short); }
+	void _sbc_izy(int v){ comp_byte(0xf1); comp_byte(v); }
+	void _sbc_izp(int v){ comp_byte(0xf2); comp_byte(v); }
+	void _sbc_zpx(int v){ comp_byte(0xf5); comp_byte(v); }
+	void _inc_zpx(int v){ comp_byte(0xf6); comp_byte(v); }
+	void _sed(int v){ comp_byte(0xf8); }
+	void _sbc_aby(int v){ comp_byte(0xf9); comp_word(v); }
+	void _plx(int v){ comp_byte(0xfa); }
+	void _sbc_abx(int v){ comp_byte(0xfd); comp_word(v); }
+	void _inc_abx(int v){ comp_byte(0xfe); comp_word(v); }
 
 
 
